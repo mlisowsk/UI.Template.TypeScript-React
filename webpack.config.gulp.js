1025639558aac04d8a26ff649eecbfb044cb2023
Martin Lisowski <martin.j.lisowski@gmail.com>
2024-03-01 20:24:20 +0100

    Further changes to webpack.config to make webpack5 build work on M-Files Desktop

--- webpack.config.gulp.js
+++ webpack.config.gulp.js
@@ -46,6 +46,30 @@ module.exports = {
 		filename: "[name].bundle.js",
 		// path: path.resolve( __dirname, "dist" )
 		iife: false,	// prevent top level IIFE on Webpack 5 - need this so M-Files finds the OnNewShellUI entry point
+		environment: {
+			arrowFunction: false,	// WebBrowser control doesn't support arrow functions
+			const: false,			// WebBrowser control doesn't support const keyword
+			module: false
+		},
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					compress: true,	// set false for debugging readability
+					format: { beautify: true },	// keep line breaks so M-Files error messages make sense (set false for production)
+					/* compress: {
+						defaults: true,
+						properties: false,		// important: don't rewrite property access using the dot notation, e.g. foo["bar"] → foo.bar
+					},*/
+					//keep_fnames: true,
+					//keep_classnames: true,
+					ie8: true,	// important for M-Files Desktop compatibility
+				},
+			}),
+		],
+		concatenateModules: true	// needed for mode development to work on desktop
 	},
 	target: "browserslist",	// need a browerslist entry in package.json specifying "ie 8", otherwise won't work in M-Files Desktop
 	externals: {
